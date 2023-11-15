8a2414c0a66704377ce54e7739747f9cd0d06e1b
Boggavarapu <S567145@nwmissouri.edu>
2023-11-14 22:11:45 -0600

    new commit

--- routes/Earring.js
+++ routes/Earring.js
@@ -1,14 +1,6 @@
 var express = require('express');
 const Earrings_controllers= require('../controllers/Earrings');
 var router = express.Router();
-/* GET detail costume page */
-router.get('/detail', Earrings_controllers.Earrings_view_one_Page);
-module.exports = router;
-/* GET create costume page */
-router.get('/create', Earrings_controllers.Earrings_create_Page);
-/* GET create update page */
-router.get('/update', Earrings_controllers.Earrings_update_Page);
-/* GET delete costume page */
-router.get('/delete', Earrings_controllers.Earrings_delete_Page);
+
 
 
--- routes/Earrings.js
+++ routes/Earrings.js
@@ -4,4 +4,13 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', Earrings_controlers.Earrings_view_all_Page);
 module.exports = router;
+/* GET detail costume page */
+router.get('/detail', Earrings_controllers.Earrings_view_one_Page);
+module.exports = router;
+/* GET create costume page */
+router.get('/create', Earrings_controlers.Earrings_create_Page);
+/* GET create update page */
+router.get('/update', Earrings_controlers.Earrings_update_Page);
+/* GET delete costume page */
+router.get('/delete', Earrings_controlers.Earrings_delete_Page);
 
